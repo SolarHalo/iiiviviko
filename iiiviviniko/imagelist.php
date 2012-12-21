0c83e38fe587d957ce3dee3f8550a0ca418313b1
superyuyue <superyuyue@126.com>
2012-12-21 11:57:24 +0800

    更换到IE9

--- iiiviviniko/imagelist.php
+++ iiiviviniko/imagelist.php
@@ -33,7 +33,7 @@ $imgList = $category->getActivits($menuInfo['id']);
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=8" /> 
+<meta http-equiv="X-UA-Compatible" content="IE=9" /> 
 <title>III VIVINIKO 时装官网</title> 
 <link href="style/style.css" type="text/css" rel="stylesheet" />
 <script type="text/javascript" src="js/jquery-1.8.0.min.js" ></script>
--- iiiviviniko/storeslocation.php
+++ iiiviviniko/storeslocation.php
@@ -39,7 +39,7 @@ $stores = $storedb->getAllStores();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-9" />
 <meta http-equiv="X-UA-Compatible" content="IE=8" /> 
 <title>III VIVINIKO 时装官网</title> 
 <link href="style/style.css" type="text/css" rel="stylesheet" />
